65c8e04427d00aa1de6bc12a0a0e28bbe93eab6b
Jake <jaketreleaven@gmail.com>
2021-02-27 19:20:30 -0500

    Wheel to change Color

--- Assets/Scripts/Color/ColorManager.cs
+++ Assets/Scripts/Color/ColorManager.cs
@@ -16,6 +16,7 @@ public enum COLOR
 public class ColorManager : Singleton<ColorManager>
 {
     private ColorControl coltroller;
+    private COLOR currentColor;
 
     #region Events
     public delegate void ChangeColor(COLOR color);
@@ -40,11 +41,37 @@ public class ColorManager : Singleton<ColorManager>
     // Start is called before the first frame update
     void Start()
     {
-        coltroller.ColorChange.Red.started += _ => { if (OnChangeColor != null) OnChangeColor(COLOR.RED); };
-        coltroller.ColorChange.Green.started += _ => { if (OnChangeColor != null) OnChangeColor(COLOR.GREEN); };
-        coltroller.ColorChange.Blue.started += _ => { if (OnChangeColor != null) OnChangeColor(COLOR.BLUE); };
-        coltroller.ColorChange.Yellow.started += _ => { if (OnChangeColor != null) OnChangeColor(COLOR.YELLOW); };
-        coltroller.ColorChange.Purple.started += _ => { if (OnChangeColor != null) OnChangeColor(COLOR.PURPLE); };
-        coltroller.ColorChange.Cyan.started += _ => { if (OnChangeColor != null) OnChangeColor(COLOR.CYAN); };
+        coltroller.ColorChange.Red.started += _ => { if (OnChangeColor != null) OnChangeColor(SetColor(COLOR.RED)); };
+        coltroller.ColorChange.Green.started += _ => { if (OnChangeColor != null) OnChangeColor(SetColor(COLOR.GREEN)); };
+        coltroller.ColorChange.Blue.started += _ => { if (OnChangeColor != null) OnChangeColor(SetColor(COLOR.BLUE)); };
+        coltroller.ColorChange.Yellow.started += _ => { if (OnChangeColor != null) OnChangeColor(SetColor(COLOR.YELLOW)); };
+        coltroller.ColorChange.Purple.started += _ => { if (OnChangeColor != null) OnChangeColor(SetColor(COLOR.PURPLE)); };
+        coltroller.ColorChange.Cyan.started += _ => { if (OnChangeColor != null) OnChangeColor(SetColor(COLOR.CYAN)); };
+
+        coltroller.ColorChange.Wheel.performed += _ =>
+        {
+            if (OnChangeColor != null)
+            {
+                int x = (int)Mathf.Clamp(coltroller.ColorChange.Wheel.ReadValue<float>(), -1, 1);
+                IncColor(x);
+                OnChangeColor(currentColor);
+            }
+        };
+    }
+
+    private COLOR SetColor(COLOR color)
+    {
+        currentColor = color;
+        return currentColor;
+    }
+    private COLOR IncColor(int i)
+    {
+        currentColor += i;
+        if ((int)currentColor > 5)
+            currentColor = COLOR.RED;
+        else if ((int)currentColor < 0)
+            currentColor = COLOR.CYAN;
+
+        return currentColor;
     }
 }
